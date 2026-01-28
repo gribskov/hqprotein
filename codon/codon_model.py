3244655e952aeb51374c14a752a7700a9ba6bacc
Michael Gribskov <mgribsko@gmail.com>
2026-01-28 12:34:18 -0500

    codon_model.py - loop over sequences works

--- codon/codon_model.py
+++ codon/codon_model.py
@@ -4,11 +4,17 @@
 # Michael Gribskov 1/28/2026
 ====================================================================================================================="""
 from include.fasta import Fasta
+from codon import Codon
 
 dnafile = 'data/z.tritici.IP0323.reannot.cds.fasta'
 cds = Fasta(filename=dnafile)
 
+codon = Codon()
+
+codon_total = 0
 while cds.next():
     print(cds.id)
+    codon_total += codon.add_from_dna(cds.seq)
+    print(f'\t=>{codon_total}')
 
 exit(0)
