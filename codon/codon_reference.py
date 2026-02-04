85ffb8f6ed4ce270b8945c21bc269f145cf7a9c6
gribskov <mgribsko@purdue.edu>
2026-02-03 19:33:39 -0500

    codon_reference.py - calculates and write codon usage as count and frequency

--- codon/codon_reference.py
+++ codon/codon_reference.py
@@ -2,7 +2,8 @@
 codon_reference.py
 
 Calculate reference codon usage tables based on file of CDS. Assumes that the input sequences are CDS (no intron or UTR)
-and that the first base is the beginning of the first codon in the coding frame.
+and that the first base is the beginning of the first codon in the coding frame, and the sequence ends at a stop codon.
+Sequence length should be multiple of three.
 
 Writes three files codon_rf0.txt, codon_rf1.txt, codon_rf2.txt
 
@@ -14,8 +15,9 @@ TODO add option of codon preference
 import argparse
 import datetime
 import textwrap as _textwrap
-from include.fasta import Fasta
+
 from codon import Codon
+from include.fasta import Fasta
 
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter):
@@ -40,6 +42,12 @@ def getopts():
                                  formatter_class=CustomFormatter)
     # cl.add_argument('--atype', type=str, default='excel',
     #                 help='annotation file type (excel)')
+
+    cl.add_argument('-c', '--count', action='store_true',
+                    help='Write raw counts (default: False')
+    cl.add_argument('-p', '--preference', action='store_true',
+                    help='Calculate codon preference instead of codon usage (default: False')
+
     # files are strings, not opened automatically by argparse
     cl.add_argument('cds_file', type=str, help='reference genome coding sequences')
     cl.add_argument('codon', type=str, help='base name for codon table names')
@@ -54,19 +62,57 @@ if __name__ == '__main__':
     current = datetime.datetime.now().strftime('%X %G.%b.%d ')
     print(f'Codon_reference - {current}')
     opt = getopts()
+
+    print(f'\tcoding sequences: {opt.cds_file}')
+    print(f'\toutput file prefix: {opt.codon}')
+    if opt.count:
+        print(f'\toutput type: counts')
+    else:
+        print(f'\toutput type: frequencies')
+
+    print('\nreading coding sequences from {opt.cds_file}')
+
     cds = Fasta(filename=opt.cds_file)
 
     # rf holds the codon information for the three reading frames
     rf = [Codon(), Codon(), Codon()]
-
     seq_n = 0
     while cds.next():
         # sum codon usage counts over all CDS sequences for all three reading frames
         seq_n += 1
-        print(f'{seq_n}\t{cds.id}')
+        print(f'\t{seq_n}\t{cds.id}')
         for frame in range(3):
             # codon counts for all three frames, includes stop codons
             rf[frame].add_from_dna(cds.seq, frame)
 
+    print(f'\nsequences read from {opt.cds_file}: {seq_n}')
+    for i in range(3):
+        print(f'\tcodons in frame {i}: {rf[i].n}')
+
+    if opt.count:
+        # print raw counts
+        for i in range(3):
+            outfile = f'{opt.codon}_rf{i}.count.txt'
+            rf[i].to_file(outfile, fieldwidth=6, decimal=0)
+
+    else:
+        # print frequencies
+        for i in range(3):
+            outfile = f'{opt.codon}_rf{i}.frequency.txt'
+            rf[i] /= rf[i].n
+            rf[i].to_file(outfile, fieldwidth=6, decimal=4)
+
+    exit(0)
+
+    # codon preference not done yet
+    familycount = []
+    for frame in range(3):
+        # calculate frequency of codon families in all three frames
+        familycount.append(rf[frame].family())
+
+    preference = []
+    for frame in range(3):
+        # codon preference for all three frames
+        preference.append(rf[frame] / familycount[frame])
 
     exit(0)
